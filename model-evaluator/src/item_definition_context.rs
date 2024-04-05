37e2a633cbfb7bf51ea7540f3d4f5f271f3f0ed4
dmntk-io <140886875+dmntk@users.noreply.github.com>
2024-04-05 20:38:12 +0200

    Unified licensing.

--- model-evaluator/src/item_definition_context.rs
+++ model-evaluator/src/item_definition_context.rs
@@ -1,35 +1,3 @@
-/*
- * DMNTK - Decision Model and Notation Toolkit
- *
- * MIT license
- *
- * Copyright (c) 2015-2023 Dariusz Depta
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- * Apache license, Version 2.0
- *
- * Copyright (c) 2015-2023 Dariusz Depta
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 //! Builder for item definition context evaluators.
 
 use crate::errors::*;
